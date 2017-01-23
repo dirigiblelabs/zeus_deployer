b04047d764f352fbcdd26269c6971e3a5215bb3d
ThuF <jordan.p.pavlov@gmail.com>
2017-01-23 13:20:26 +0200

    Updates cluster utils

--- zeus_deployer/ScriptingServices/zeus/utils/cluster.js
+++ zeus_deployer/ScriptingServices/zeus/utils/cluster.js
@@ -7,12 +7,7 @@ const EXT_POINT_NAME_SETTINGS_PROVIDER = '/zeus/deployer/extension_points/cluste
 const EXT_POINT_NAME_CLUSTER_APPLICATION_HOOKS = '/zeus/deployer/extension_points/clusterApplicationHooks';
 
 exports.getSettings = function() {
-	var clusterSettingsProvider = getClusterSettingsProviderExtension();
-	return {
-		'server': getClusterSettingsServer(clusterSettingsProvider),
-		'token': getClusterSettingsToken(clusterSettingsProvider),
-		'namespace': getClusterSettingsNamespace(clusterSettingsProvider)
-	};
+	return getClusterSettings(getClusterSettingsProviderExtension());
 };
 
 exports.afterCreateApplication = function(application) {
@@ -33,16 +28,8 @@ exports.afterDeleteApplication = function(application) {
 	}
 };
 
-function getClusterSettingsServer(clusterSettingsProvider) {
-	return clusterSettingsProvider && isFunction(clusterSettingsProvider.getServer) ? clusterSettingsProvider.getServer() : null;
-}
-
-function getClusterSettingsToken(clusterSettingsProvider) {
-	return clusterSettingsProvider && isFunction(clusterSettingsProvider.getToken) ? clusterSettingsProvider.getToken() : null;
-}
-
-function getClusterSettingsNamespace(clusterSettingsProvider) {
-	return clusterSettingsProvider && isFunction(clusterSettingsProvider.getNamespace) ? clusterSettingsProvider.getNamespace() : null;
+function getClusterSettings(clusterSettingsProvider) {
+	return clusterSettingsProvider && isFunction(clusterSettingsProvider.getSettings) ? clusterSettingsProvider.getSettings() : {};
 }
 
 function getClusterSettingsProviderExtension() {
